56c155456401a80170786952b836f6d9c036e6c7
adrrob1002 <adrianrobinson101@gmail.com>
2022-08-09 21:12:39 +0300

    Fix package removal

--- src/server/services/character-service.ts
+++ src/server/services/character-service.ts
@@ -6,23 +6,20 @@ import { Players } from "@rbxts/services";
 export class CharacterService implements OnStart {
 	onStart() {
 		Players.PlayerAdded.Connect((player) => {
-			if (player.Character) this.onCharacterAddedCallback(player.Character);
-			player.CharacterAdded.Connect((character) => this.onCharacterAddedCallback(character));
+			player.CharacterAppearanceLoaded.Connect((character) => this.onCharacterAppearanceLoaded(character));
 		});
 	}
 
-	async onCharacterAddedCallback(char: Model) {
+	async onCharacterAppearanceLoaded(char: Model) {
 		const currentCharacter = await promiseR6(char);
 
-		task.delay(0.1, () => {
-			const humanoidDescription = currentCharacter.Humanoid.GetAppliedDescription();
-			humanoidDescription.Head = 0;
-			humanoidDescription.Torso = 0;
-			humanoidDescription.RightLeg = 0;
-			humanoidDescription.LeftLeg = 0;
-			humanoidDescription.RightArm = 0;
-			humanoidDescription.LeftArm = 0;
-			currentCharacter.Humanoid.ApplyDescription(humanoidDescription);
-		});
+		const humanoidDescription = currentCharacter.Humanoid.GetAppliedDescription();
+		humanoidDescription.Head = 0;
+		humanoidDescription.Torso = 0;
+		humanoidDescription.RightLeg = 0;
+		humanoidDescription.LeftLeg = 0;
+		humanoidDescription.RightArm = 0;
+		humanoidDescription.LeftArm = 0;
+		currentCharacter.Humanoid.ApplyDescription(humanoidDescription);
 	}
 }
